d7da0b223c6213ded7da4351ecac69533b3be4de
Amirammi <amirammina@gmail.com>
2023-09-14 15:09:52 +0300

    Added Book model to models.py & register in admin.py

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "cover", "inventory", "daily_fee")
+    list_filter = ("author", )
+    search_fields = ("title", )
--- book/models.py
+++ book/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [("H", "HARD"), ("S", "SOFT")]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=1, choices=COVER_CHOICES)
+    inventory = models.IntegerField(validators=(MinValueValidator(limit_value=0)))
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
