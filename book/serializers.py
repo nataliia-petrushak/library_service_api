4d96409184240095861492dc22e13c9cbc9c8131
Amirammi <amirammina@gmail.com>
2023-09-14 15:23:02 +0300

    Add BookSerializer & BookListSerializer to serializers.py

---
+++ book/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee"
+        )
+
+
+class BookListSerializer(BookSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "daily_fee")
