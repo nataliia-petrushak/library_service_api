adffcd5aa419b38c4bf2e8c3793d9f86a477af62
Amirammi <amirammina@gmail.com>
2023-09-14 18:02:20 +0300

    Add rest_framework_simplejwt to INSTALLED_APPS in settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "book",
     "user",
 ]
@@ -126,7 +127,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
