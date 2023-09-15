77326758a4766a6dc616735c93b1b975ceba456f
Amirammi <amirammina@gmail.com>
2023-09-15 16:26:31 +0300

    Add BorrowingSerializer & DetailSerializer to serializers.py

---
+++ borrowing/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from .models import Borrowing
+from book.serializers import BookSerializer
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book_id",
+            "user_id"
+        )
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookSerializer()
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+        )
