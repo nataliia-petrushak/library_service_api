c999110fffd92d73f20c5f6c94d78fcbd697f800
Amirammi <amirammina@gmail.com>
2023-09-15 16:27:24 +0300

    Add BorrowingReadView to get borrowings list & detail in views.py

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import BorrowingSerializer, BorrowingDetailSerializer
+from .models import Borrowing
+
+
+class BorrowingReadView(
+    generics.ListAPIView,
+    generics.RetrieveAPIView,
+    viewsets.GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return self.serializer_class
