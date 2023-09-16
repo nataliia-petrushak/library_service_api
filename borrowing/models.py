6fb7c797a010766c3cd74fc862cd5cf18e6a3291
Amirammi <amirammina@gmail.com>
2023-09-16 17:05:07 +0300

    Add validation for the book inventory

--- borrowing/models.py
+++ borrowing/models.py
@@ -12,8 +12,16 @@ class Borrowing(models.Model):
     book_id = models.IntegerField()
     user_id = models.IntegerField()
 
+    @property
     def book(self) -> Book:
         return get_object_or_404(Book, pk=self.book_id)
 
+    @property
     def user(self) -> User:
         return get_object_or_404(User, pk=self.user_id)
+
+    @staticmethod
+    def validate_inventory(book_id, error_to_raise):
+        book = get_object_or_404(Book, pk=book_id)
+        if book.inventory < 1:
+            raise error_to_raise("There are no books in inventory to borrow")
--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Borrowing
 from book.serializers import BookSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    borrow_date = serializers.DateField(format="%Y-%m-%d", read_only=True)
+    actual_return_date = serializers.DateField(format="%Y-%m-%d", read_only=True)
+    expected_return_date = serializers.DateField(format="%Y-%m-%d")
+
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        Borrowing.validate_inventory(
+            attrs["book_id"],
+            ValidationError
+        )
+        return data
 
     class Meta:
         model = Borrowing
