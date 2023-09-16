cdd682bff0f9cfbd64ec4e1fb74eac15c19821c6
Amirammi <amirammina@gmail.com>
2023-09-16 19:09:30 +0300

    Add params_to_int method & add filters for borrowing queryset

--- borrowing/views.py
+++ borrowing/views.py
@@ -18,10 +18,26 @@ class BorrowingViewSet(
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user.id)
 
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return self.queryset
-        return self.queryset.filter(user_id=self.request.user.id)
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if not self.request.user.is_staff:
+            self.queryset = self.queryset.filter(user_id=self.request.user.id)
+
+        if is_active == "True":
+            self.queryset = self.queryset.filter(actual_return_date__isnull=True)
+
+        if user_id and self.request.user.is_staff:
+            user_id = self._params_to_ints(user_id)
+            self.queryset = self.queryset.filter(user_id__in=user_id)
+
+        return self.queryset
 
     def get_serializer_class(self):
         if self.action in ["retrieve", "update", "partial_update"]:
