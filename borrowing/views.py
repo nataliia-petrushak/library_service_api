72b98730c698ee97a71b90bddef2b3c73570bf23
Amirammi <amirammina@gmail.com>
2023-09-16 17:09:29 +0300

    add perform_create & get_queryset methods & changed get_serializer_class

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,20 +1,29 @@
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
 
-from .serializers import BorrowingSerializer, BorrowingDetailSerializer
+from .permissions import IsAuthenticatedOrIsAdmin
+from .serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer
+)
 from .models import Borrowing
 
 
-class BorrowingReadView(
-    generics.ListAPIView,
-    generics.RetrieveAPIView,
-    viewsets.GenericViewSet
+class BorrowingViewSet(
+    viewsets.ModelViewSet
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrIsAdmin,)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return self.queryset
+        return self.queryset.filter(user_id=self.request.user.id)
 
     def get_serializer_class(self):
-        if self.action == "retrieve":
+        if self.action in ["retrieve", "update", "partial_update"]:
             return BorrowingDetailSerializer
         return self.serializer_class
