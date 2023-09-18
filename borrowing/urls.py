187b511e9dbe31939f3f887d846af93b0bca0799
Amirammi <amirammina@gmail.com>
2023-09-18 14:29:41 +0300

    Added endpoint for returning borrowing

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import BorrowingViewSet
+from .views import BorrowingViewSet, return_borrowing
 
 router = routers.DefaultRouter()
 router.register("", BorrowingViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("<int:pk>/return/", return_borrowing, name="return-borrowing")
+              ] + router.urls
 
 
 app_name = "borrowing"
