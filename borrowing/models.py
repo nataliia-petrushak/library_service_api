c903500e0752a6b2510e1a0f80bdea84e793d2f1
Amirammi <amirammina@gmail.com>
2023-09-18 19:02:18 +0300

    Implemented send_borrowing_notification feature in models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,4 +1,8 @@
+import requests
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework.generics import get_object_or_404
 
 from book.models import Book
@@ -25,3 +29,18 @@ class Borrowing(models.Model):
         book = get_object_or_404(Book, pk=book_id)
         if book.inventory < 1:
             raise error_to_raise("There are no books in inventory to borrow")
+
+
+@receiver(post_save, sender=Borrowing)
+def send_borrowing_notification(sender, instance, created, **kwargs):
+    book = get_object_or_404(Book, pk=instance.book_id)
+    if created:
+        message = f"You have borrowed {book.title}." \
+                  f"\nExpected return date:" \
+                  f"\n{instance.expected_return_date}\n" \
+                  f"Price:\n" \
+                  f"{book.daily_fee} $"
+        chat_id = settings.CHAT_ID,
+        url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
+        data = {"chat_id": chat_id, "text": message}
+        requests.post(url, data)
