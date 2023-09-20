12435418bcff64c37b62f61d2fe685d28d2cf3cb
Amirammi <amirammina@gmail.com>
2023-09-20 20:22:37 +0300

    Add amount_of_inventory in book models.py for increase & decrease inventory

--- book/models.py
+++ book/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from rest_framework.generics import get_object_or_404
 
 
 class Book(models.Model):
@@ -10,3 +11,12 @@ class Book(models.Model):
     cover = models.CharField(max_length=1, choices=COVER_CHOICES)
     inventory = models.IntegerField(validators=[MinValueValidator(limit_value=0)])
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+def amount_of_inventory(book_id, increase=False):
+    book = get_object_or_404(Book, pk=book_id)
+    if increase:
+        book.inventory += 1
+    else:
+        book.inventory -= 1
+    book.save()
