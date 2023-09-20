b614e19dea518ff322f7fd030981101576b9d860
Amirammi <amirammina@gmail.com>
2023-09-20 19:02:06 +0300

    Made status & type choices more readable & delete borrowing property

--- payment/models.py
+++ payment/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 
-from borrowing.models import Borrowing
 from rest_framework.generics import get_object_or_404
 
 
 class Payment(models.Model):
-    STATUS_CHOICE = [("PN", "PENDING"), ("PD", "PAID")]
-    TYPE_CHOICE = [("P", "PAYMENT"), ("F", "FINE")]
+    STATUS_CHOICE = [("PENDING", "Pending"), ("PAID", "Paid")]
+    TYPE_CHOICE = [("PAYMENT", "Payment"), ("FINE", "Fine")]
 
-    status = models.CharField(max_length=2, choices=STATUS_CHOICE)
-    type = models.CharField(max_length=1, choices=TYPE_CHOICE)
+    status = models.CharField(max_length=7, choices=STATUS_CHOICE)
+    type = models.CharField(max_length=7, choices=TYPE_CHOICE)
     borrowing_id = models.IntegerField()
     user_id = models.IntegerField()
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def borrowing(self) -> Borrowing:
-        return get_object_or_404(Borrowing, pk=self.borrowing_id)
