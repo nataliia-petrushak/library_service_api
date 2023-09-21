68468c0c2b1d806732f555e9689af6d4b220808b
Amirammi <amirammina@gmail.com>
2023-09-21 15:46:10 +0300

    Created success_payment & cancel_payment functions

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,9 @@
-from rest_framework import generics, viewsets
+import stripe
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Payment
 from .serializers import (
@@ -32,3 +36,22 @@ class PaymentViewSet(
         if self.action == "retrieve":
             return PaymentDetailSerializer
         return self.serializer_class
+
+
+@api_view(["GET"])
+def success_payment(request):
+    session_id = request.args.get("session_id")
+    session = stripe.checkout.Session.retrieve(session_id)
+
+    if session.payment_status == "paid":
+        payment = get_object_or_404(Payment, session_id=session_id)
+        payment.status = "PAID"
+        payment.save()
+
+
+@api_view(["GET"])
+def cancel_payment(request):
+    return Response({
+        "message": "Payment failed. "
+                   "Please proceed with the payment within 24 hours"
+    })
