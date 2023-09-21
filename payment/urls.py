4c165bf544299d888c523069a3d56a5c4aed029b
Amirammi <amirammina@gmail.com>
2023-09-21 15:46:36 +0300

    Added endpoints for Success & cancel functions

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,14 @@
+from django.urls import path
 from rest_framework import routers
 
-from .views import PaymentViewSet
+from .views import PaymentViewSet, success_payment, cancel_payment
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("success/", success_payment, name="success-payment"),
+    path("cancel/", cancel_payment, name="cancel-payment")
+] + router.urls
 
 app_name = "payment"
