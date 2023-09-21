d5debb312bffce6519ebc80762c26771e2b0a1d0
Amirammi <amirammina@gmail.com>
2023-09-21 15:45:15 +0300

    Fixed TELEGRAM_CHAT_ID in send_notification.py

--- borrowing/management/commands/send_notification.py
+++ borrowing/management/commands/send_notification.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def notification(message):
-    chat_id = settings.CHAT_ID,
+    chat_id = settings.TELEGRAM_CHAT_ID,
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     requests.post(url, data)
