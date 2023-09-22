0fda5fe5ba6347cbfad70434f86e4bed409bb5d4
Amirammi <amirammina@gmail.com>
2023-09-22 17:10:17 +0300

    Added StripeSession model in payment/models.py

--- payment/models.py
+++ payment/models.py
@@ -12,3 +12,25 @@ class Payment(models.Model):
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class StripeSession(models.Model):
+    session_id = models.CharField(max_length=255)
+    payment_id = models.IntegerField()
+    user_id = models.IntegerField()
+    expiration_time = models.DateTimeField()
+    is_expired = models.BooleanField(default=False)
+
+    @property
+    def payment(self) -> Payment:
+        return get_object_or_404(Payment, pk=self.payment_id)
+
+    def cancel_payment_session(self):
+        stripe.checkout.Session.expire(self.session_id)
+        payment = self.payment
+
+        self.is_expired = True
+        self.save()
+
+        payment.status = "EXPIRED"
+        payment.save()
