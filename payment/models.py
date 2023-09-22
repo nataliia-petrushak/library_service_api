9a3f53fbc2eefdb6083e5f6d4e884c1ab0c4444f
Amirammi <amirammina@gmail.com>
2023-09-22 17:16:09 +0300

    Add EXPIRED to STATUS_CHOICE in models.py

--- payment/models.py
+++ payment/models.py
@@ -1,15 +1,21 @@
+import stripe
 from django.db import models
+from rest_framework.generics import get_object_or_404
 
 
 class Payment(models.Model):
-    STATUS_CHOICE = [("PENDING", "Pending"), ("PAID", "Paid")]
+    STATUS_CHOICE = [
+        ("PENDING", "Pending"),
+        ("PAID", "Paid"),
+        ("EXPIRED", "Expired")
+    ]
     TYPE_CHOICE = [("PAYMENT", "Payment"), ("FINE", "Fine")]
 
     status = models.CharField(max_length=7, choices=STATUS_CHOICE)
     type = models.CharField(max_length=7, choices=TYPE_CHOICE)
     borrowing_id = models.IntegerField()
     user_id = models.IntegerField()
-    session_url = models.URLField()
+    session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
 
