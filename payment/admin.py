5b40a468032cbcfe154bda6955377b70d53ce7b2
Amirammi <amirammina@gmail.com>
2023-09-22 17:10:33 +0300

    Registered model in admin.py

--- payment/admin.py
+++ payment/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from .models import Payment
+from .models import Payment, StripeSession
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ("id", "status", "type", "borrowing_id", "user_id", "money_to_pay")
+    list_display = (
+        "id", "status", "type", "borrowing_id", "user_id", "money_to_pay"
+    )
     list_filter = ("status", "type")
     search_fields = ("borrowing_id", "user_id")
+
+
+@admin.register(StripeSession)
+class StripeSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "session_id", "user_id", "expiration_time", "is_expired"
+    )
+    list_filter = ("is_expired", )
+    search_fields = ("expiration_time",)
